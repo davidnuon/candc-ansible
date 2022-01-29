d800f200a0bfcea0769b12216005a86d0731617a
David Nuon <davidnuongm@gmail.com>
2022-01-28 21:31:50 -0800

    Add playbooks

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # candc-ansible
+
+## Setup
+
+1. Run `bootstrap.sh`
+2. Run `ansible-playbook --ask-become-pass main.yml`
---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assume sudo but keep track of 
+# current user
+if [[ $EUID != 0 ]]; then
+	sudo "$0" `whoami`
+	exit $?
+fi
+
+CURRENT_USER=$1
+
+echo "Grab updates..."
+apt-get update
+
+echo "Installing tools..."
+apt-get install -y python3-pip
+
+echo "Installing ansible..."
+pip3 install ansible
